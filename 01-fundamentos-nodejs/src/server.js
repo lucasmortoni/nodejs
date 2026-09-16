5e16eee285edcaf29dd44852bafa5a661c8815df
Lucas Mortoni <lucasmortoni@outlook.com>
2026-09-16 00:47:51 -0300

    feat(01-fundamentos-nodejs): add route checking and redirecting

--- 01-fundamentos-nodejs/src/server.js
+++ 01-fundamentos-nodejs/src/server.js
@@ -1,9 +1,16 @@
-import http from 'node:http'
+import http from "node:http";
 
 const server = http.createServer((req, res) => {
-  return res.end('Hello world, Lucas!')
-})
+  const { method, url } = req;
+
+  if (method === "GET" && url === "/users") {
+    return res.end("List of users");
+  }
+  if (method === "POST" && url === "/users") {
+    return res.end("Creating a new user");
+  }
+});
 
 server.listen(3333, () => {
-  console.log('Server is running on port 3333')
-})
+  console.log("Server is running on port 3333");
+});
